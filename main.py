c76017cdeab1e40cc2952929aa6f073580117f22
mmr0429 <maciej@lenovo-g50-80>
2019-06-01 18:27:12 +0200

    Worked on save & save as

--- main.py
+++ main.py
@@ -38,44 +38,52 @@ text_area.focus_set() #focus to the text area
 
 path_to_save=""
 
+def cleararea():
+    text_area.delete('1.0', END)
+
+
 def new():
     window.title("New File")
     global is_new
     is_new = 1
     #print(is_new)
+    cleararea()
 
 def open():
-    print("Opening")
+    #print("Opening")
+    cleararea()
     window.filename = filedialog.askopenfilename(initialdir = "~",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
     window.title(window.filename)
     f_content=fread(str(window.filename))
     global is_new
     is_new = 0
-
-    print(window.filename)
-    print(f_content)
+    global path_to_save
+    path_to_save=str(window.filename)
+    #print(window.filename)
+    #print(f_content)
     text_area.insert("1.0",f_content)
 
 
 def save_as():
-    print("Saving as")
+    #print("Saving as")
     save_as_dir=filedialog.asksaveasfilename(initialdir="~",defaultextension=".txt",title = "Select save location",filetypes=(("text files","*txt"),("all files", "*.*")))
 
-    print(save_as_dir)
+    #print(save_as_dir)
     global path_to_save
     path_to_save=save_as_dir
     acontent=text_area.get("1.0", "end-1c")
-    print(acontent)
+    #print(acontent)
     fwrite(save_as_dir,acontent)
     global is_new
     is_new=0
 
 def save():
-    print("Saving")
-    if is_new is 1:
+    #print("Saving")
+    global path_to_save
+    if (is_new is 1) or (path_to_save is '') :
         save_as()
     else:
-        global path_to_save
+        #global path_to_save
         acontent=text_area.get("1.0", "end-1c")
         fwrite(path_to_save,acontent)
 
@@ -86,7 +94,6 @@ menu_bar.add_command(label="Open", command=open)
 menu_bar.add_command(label="Save", command=save)
 menu_bar.add_command(label="Save as", command=save_as)
 window.config(menu=menu_bar)
-
 
 
 
